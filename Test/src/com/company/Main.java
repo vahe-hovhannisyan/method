8c45a0319631f079b606ca0e017ac59f62d5a3e0
vahe-hovhannisyan <98690049+vahe-hovhannisyan@users.noreply.github.com>
2022-01-30 18:29:30 +0400

    Update Main.java

--- Test/src/com/company/Main.java
+++ Test/src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(sum(12));
+        System.out.println(sum(12)); 
     }
     public static int sum(int num){
         return num * num;
