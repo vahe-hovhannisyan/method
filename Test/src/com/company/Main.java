fa833d080f6c8f44deb1faa9c5b77b9ca5402402
vahe-hovhannisyan <98690049+vahe-hovhannisyan@users.noreply.github.com>
2022-01-30 18:58:45 +0400

    Update Main.java

--- Test/src/com/company/Main.java
+++ Test/src/com/company/Main.java
@@ -3,10 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(sum(12)); 
+        System.out.println(min(1, 5, 7));
     }
-    public static int sum(int num){
-        return num * num;
 
+    public static int min(int num, int num1, int num2) {
+        int min = 0;
+        if (num <= num1 && num <= num2) {
+            min = num;
+        } else if (num1 <= num && num1 <= num2) {
+            min = num1;
+        } else if (num2 <= num && num2 <= num1) {
+            min = num2;
+        }
+        return min;
     }
 }
