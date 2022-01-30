ebe9667569e9740ac03faa433fbdc31bf4395dea
vahe-hovhannisyan <98690049+vahe-hovhannisyan@users.noreply.github.com>
2022-01-30 18:21:40 +0400

    Test

--- Test/src/com/company/Main.java
+++ Test/src/com/company/Main.java
@@ -3,9 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(sum(12));
+    }
+    public static int sum(int num){
+        return num * num;
 
-        System.out.println("Vahram");
-        // asjdflaskjdfhlaksdf
-        //jijiojoij
     }
 }
