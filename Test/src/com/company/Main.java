c6bb238d18e5937c0ed5cbab0a9d8aa384c6e6d3
vahe-hovhannisyan <98690049+vahe-hovhannisyan@users.noreply.github.com>
2022-01-30 19:53:05 +0400

    Update Main.java

--- Test/src/com/company/Main.java
+++ Test/src/com/company/Main.java
@@ -3,9 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(min(5, 1, 8));
-        System.out.println(square(12));
-        System.out.println(sum(4, 5, 6));
+        System.out.println(min(5, 1, 8) + " min ");
+        System.out.println(square(12) + " square ");
+        System.out.println(sum(4, 5, 6) + " sum ");
+        System.out.println(average(3,7,9) + " average ");
+        System.out.println(multiple(1,5,-4) + " multiple ");
 
     }
 
@@ -15,7 +17,7 @@ public class Main {
             min = num;
         } else if (num1 <= num && num1 <= num2) {
             min = num1;
-        } else{
+        } else {
             min = num2;
         }
         return min;
@@ -24,7 +26,15 @@ public class Main {
     public static int square(int num) {
         return num * num;
     }
-    public static int sum(int num, int num1, int num2){
+
+    public static int sum(int num, int num1, int num2) {
         return num + num1 + num2;
     }
+    public static int average(int num, int num1, int num2){
+        return (num + num1 + num2) / 3;
+    }
+    public static int multiple(int num, int num1, int num2){
+        return num * num1 * num2;
+    }
+
 }
