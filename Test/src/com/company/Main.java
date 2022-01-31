38fed846e162838935c8fc6a6189a671b41ff3b9
vahe-hovhannisyan <98690049+vahe-hovhannisyan@users.noreply.github.com>
2022-01-31 22:59:33 +0400

    create method which print number + String

--- Test/src/com/company/Main.java
+++ Test/src/com/company/Main.java
@@ -6,9 +6,9 @@ public class Main {
         System.out.println(min(5, 1, 8) + " min ");
         System.out.println(square(12) + " square ");
         System.out.println(sum(4, 5) + " sum ");
-        System.out.println(average(3,7) + " average ");
-        System.out.println(multiply(1,5) + " multiply ");
-
+        System.out.println(average(3, 7) + " average ");
+        System.out.println(multiply(1, 5) + " multiply ");
+        System.out.println(string(7 + " " + "text"););
     }
 
     public static int min(int num, int num1, int num2) {
@@ -30,11 +30,21 @@ public class Main {
     public static int sum(int num, int num1) {
         return num + num1;
     }
-    public static int average(int num, int num1){
+
+    public static int average(int num, int num1) {
         return (num + num1) / 2;
     }
-    public static int multiply(int num, int num1){
+
+    public static int multiply(int num, int num1) {
         return num * num1;
     }
 
-}
+    public static int string(int num, String string) {
+        System.out.println(string(5 + " " + "text"));
+        return num;
+
+    }
+
+    }
+
+
